dc134540e9a20b75655d4d8277ba72a4e6af01d9
franziska <franziska.gonschor@web.de>
2018-05-10 20:05:54 +0200

    Question.js definiert, Layout fuer Quiz festgelegt

--- Server/Server.js
+++ Server/Server.js
@@ -43,7 +43,7 @@ app.listen(app.get('port'), function () {
 var http = require('http');
 
 var server = http.createServer(function(req, res){
-    console.log('request was made:' + req.url); 
+    console.log('request was made:' + req.url);
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('hey');
 });
